e1bf4b3997c9d44797dadbca4962bb88c2481d00
Mar <mar@strawmelonjuice.com>
2023-11-23 20:07:42 +0100

    moving to lazyvim bc im lazy (im not)

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+{ "rose-pine/neovim", name = "rose-pine" },
+{ "EdenEast/nightfox.nvim" },
+{ "tanvirtin/monokai.nvim" },
+
+
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "monokai_ristretto",
+    },
+  }
+}
