2bbfefb66b01a572c24566a4af88d9ca0586db32
Mar <mar@strawmelonjuice.com>
2023-11-24 16:06:01 +0100

    Update config

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,16 @@
 return {
-  {"catppuccin/nvim", name = "catppuccin", priority = 1000},
-  {"rose-pine/neovim", name = "rose-pine"},
-  {"EdenEast/nightfox.nvim"},
-  {"tanvirtin/monokai.nvim"},
-  {"ellisonleao/gruvbox.nvim"},
+  { "catppuccin/nvim",         name = "catppuccin", priority = 1000 },
+  { "rose-pine/neovim",        name = "rose-pine" },
+  { "EdenEast/nightfox.nvim" },
+  { "tanvirtin/monokai.nvim" },
+  { "ellisonleao/gruvbox.nvim" },
+  { "sts10/vim-pink-moon" },
+  { "rktjmp/lush.nvim" },
+  { "Scysta/pink-panic.nvim" },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "monokai_ristretto",
+      colorscheme = "pink-moon",
     },
   }
 }
