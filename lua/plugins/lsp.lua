3fc8d11dfcf795073e88017f5db66fce40120f48
Mar <mar@strawmelonjuice.com>
2024-06-14 20:54:13 +0200

    make it go a lil smoother

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -74,56 +74,56 @@ return {
         })
       end)
     end,
-    config = function()
-      ---@class ConformOpts
-      local opts = {
-        -- LazyVim will use these options when formatting with the conform.nvim formatter
-        format = {
-          timeout_ms = 3000,
-          async = false,       -- not recommended to change
-          quiet = false,       -- not recommended to change
-          lsp_fallback = true, -- not recommended to change
-        },
-        ---@type table<string, conform.FormatterUnit[]>
-        formatters_by_ft = {
-          lua = { "stylua" },
-          fish = { "fish_indent" },
-          sh = { "shfmt" },
-          javascript = { "biome", "prettier" },
-          typescript = { "biome", "prettier" },
-          vue = { "prettier" },
-          css = { "prettier" },
-          scss = { "prettier" },
-          less = { "prettier" },
-          html = { "prettier" },
-          json = { "prettier" },
-          jsonc = { "prettier" },
-          yaml = { "prettier" },
-          markdown = { "prettier" },
-          graphql = { "prettier" },
-          handlebars = { "prettier" },
-          rust = { "rustfmt", "rust-analyzer" }
+    opts = {
+      -- LazyVim will use these options when formatting with the conform.nvim formatter
+      format = {
+        timeout_ms = 3000,
+        async = false,       -- not recommended to change
+        quiet = false,       -- not recommended to change
+        lsp_fallback = true, -- not recommended to change
+      },
+      ---@type table<string, conform.FormatterUnit[]>
+      formatters_by_ft = {
+        lua = { "stylua" },
+        fish = { "fish_indent" },
+        sh = { "shfmt" },
+        javascript = { "biome", "prettier" },
+        typescript = { "biome", "prettier" },
+        vue = { "prettier" },
+        css = { "prettier" },
+        scss = { "prettier" },
+        less = { "prettier" },
+        html = { "prettier" },
+        json = { "prettier" },
+        jsonc = { "prettier" },
+        yaml = { "prettier" },
+        markdown = { "prettier" },
+        graphql = { "prettier" },
+        handlebars = { "prettier" },
+        rust = { "rustfmt", "rust-analyzer" }
 
-        },
-        -- The options you set here will be merged with the builtin formatters.
-        -- You can also define any custom formatters here.
-        ---@type table<string, conform.FormatterConfigOverride|fun(bufnr: integer): nil|conform.FormatterConfigOverride>
-        formatters = {
-          injected = { options = { ignore_errors = true } },
-          -- # Example of using dprint only when a dprint.json file is present
-          -- dprint = {
-          --   condition = function(ctx)
-          --     return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[2]
-          --   end,
-          -- },
-          --
-          -- # Example of using shfmt with extra args
-          -- shfmt = {
-          --   prepend_args = { "-i", "2", "-ci" },
-          -- },
-        },
-      }
-      return opts
-    end,
+      },
+      -- The options you set here will be merged with the builtin formatters.
+      -- You can also define any custom formatters here.
+      ---@type table<string, conform.FormatterConfigOverride|fun(bufnr: integer): nil|conform.FormatterConfigOverride>
+      formatters = {
+        injected = { options = { ignore_errors = true } },
+        -- # Example of using dprint only when a dprint.json file is present
+        -- dprint = {
+        --   condition = function(ctx)
+        --     return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[2]
+        --   end,
+        -- },
+        --
+        -- # Example of using shfmt with extra args
+        -- shfmt = {
+        --   prepend_args = { "-i", "2", "-ci" },
+        -- },
+      },
+      init = function()
+        -- If you want the formatexpr, here is the place to set it
+        vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+      end,
+    }
   }
 }
