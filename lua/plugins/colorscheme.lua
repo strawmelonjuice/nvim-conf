ffeb1fe63243917271286a6ad8092fc0e97e2b91
Mar <mar@strawmelonjuice.com>
2024-08-01 18:52:56 +0200

    Inline that.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,13 +6,11 @@ local get_default_random = function()
   math.randomseed(os.time())
   math.random(); math.random(); math.random();
   local random = math.ceil((math.random(0.1, 1.9) / 2)) - 1;
-
   vim.notify_once("[Random colorscheme]: random = " .. random .. " - " .. (random == 1 and "light" or "dark"))
   if random == 1 then return default_light end
   if random == 0 then return default_dark end
   return "default"
 end;
-local default_random = get_default_random()
 return {
   { "rose-pine/neovim",        name = "rose-pine" },
   { "catppuccin/nvim",         name = "catppuccin" },
@@ -25,7 +23,8 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = default_random },
+      colorscheme = get_default_random()
+    },
     keys = {
       -- { "<C-M>d",     function() vim.cmd("colorscheme " .. default_dark) end,  mode = { "n", "t" }, desc = "Set to default dark mode theme." },
       { "<leader>Md", function() vim.cmd("colorscheme " .. default_dark) end,  mode = { "n", "t" }, desc = "Set to default dark mode theme." },
