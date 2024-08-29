fcb1735a79789519605618bde970316f958ad47c
Mar <mar@strawmelonjuice.com>
2024-08-29 11:38:26 +0200

    Colorscheme greened

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,6 @@
 vim.opt.termguicolors = true
-local default_light = "peachpuff"
--- local default_light = "rose-pine-dawn"
+-- local default_light = "peachpuff"
+local default_light = "kanagawa-lotus"
 -- local default_dark = "yellow-moon"
 local default_dark = "fluoromachine"
 -- local default_dark = "rose-pine-main"
@@ -42,8 +42,110 @@ local get_colorscheme = function(a)
   end
 end;
 return {
-  { "rose-pine/neovim",        name = "rose-pine" },
-  { "catppuccin/nvim",         name = "catppuccin" },
+  {
+    "rebelot/kanagawa.nvim",
+    name = "kanagawa",
+    config = function()
+      require('kanagawa').setup({
+        compile = false,  -- enable compiling the colorscheme
+        undercurl = true, -- enable undercurls
+        commentStyle = { italic = true },
+        functionStyle = {},
+        keywordStyle = { italic = true },
+        statementStyle = { bold = true },
+        typeStyle = {},
+        transparent = false,   -- do not set background color
+        dimInactive = false,   -- dim inactive window `:h hl-NormalNC`
+        terminalColors = true, -- define vim.g.terminal_color_{0,17}
+        colors = {             -- add/modify theme and palette colors
+          palette = {},
+          theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+        },
+        overrides = function(colors) -- add/modify highlights
+          return {}
+        end,
+        theme = "wave",  -- Load "wave" theme when 'background' option is not set
+        background = {   -- map the value of 'background' option to a theme
+          dark = "wave", -- try "dragon" !
+          light = "lotus"
+        },
+      })
+    end,
+  },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function()
+      require('rose-pine').setup({
+        --- @usage 'auto'|'main'|'moon'|'dawn'
+        variant = 'dawn',
+        --- @usage 'main'|'moon'|'dawn'
+        dark_variant = 'moon',
+        bold_vert_split = false,
+        dim_nc_background = true,
+        disable_background = true,
+        disable_float_background = false,
+        disable_italics = false,
+
+        --- @usage string hex value or named color from rosepinetheme.com/palette
+        groups = {
+          background = 'base',
+          background_nc = '_experimental_nc',
+          panel = 'surface',
+          panel_nc = 'base',
+          border = 'highlight_med',
+          comment = 'muted',
+          link = 'iris',
+          punctuation = 'subtle',
+
+          error = 'love',
+          hint = 'iris',
+          info = 'foam',
+          warn = 'gold',
+
+          headings = {
+            h1 = 'iris',
+            h2 = 'foam',
+            h3 = 'rose',
+            h4 = 'gold',
+            h5 = 'pine',
+            h6 = 'foam',
+          }
+          -- or set all headings at once
+          -- headings = 'subtle'
+        },
+
+        -- Change specific vim highlight groups
+        -- https://github.com/rose-pine/neovim/wiki/Recipes
+        highlight_groups = {
+          ColorColumn = { bg = 'rose' },
+
+          -- Blend colours against the "base" background
+          CursorLine = { bg = 'foam', blend = 10 },
+          StatusLine = { fg = 'love', bg = 'love', blend = 10 },
+        }
+      })
+    end,
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "latte",
+        default_integrations = true,
+        integrations = {
+          treesitter = true,
+          telescope = true,
+          nvimtree = true,
+          noice = true,
+        },
+        term_colors = true,
+        transparent_background = true,
+      })
+    end,
+  },
   { "EdenEast/nightfox.nvim" },
   { "tanvirtin/monokai.nvim" },
   { "ellisonleao/gruvbox.nvim" },
